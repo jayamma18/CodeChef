8def424c12ee6f94c613f2f3307172a27ef16abd
jayamma18 <sakajayamma7@gmail.com>
2026-08-31 15:52:42 +0530

    ✅ VBHXB05 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/VBHXB05/solution.cpp
@@ -0,0 +1,14 @@
+console.log(currentHour);
+
+// complete your code from here
+}else if(currentHour < 17) {
+    greeting = "Good Morning!";
+if (currentHour < 12) {
+
+let greeting;
+
+    greeting= "Good Afternoon!"
+}else {
+    greeting= "Good Evening!"
+}
+console.log(greeting);
