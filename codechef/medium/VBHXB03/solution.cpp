779df88a9678f4ede644cb5debd0ae406cd2666c
jayamma18 <sakajayamma7@gmail.com>
2026-08-31 15:47:49 +0530

    ✅ VBHXB03 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ codechef/medium/VBHXB03/solution.cpp
@@ -0,0 +1,8 @@
+// Define the Celsius temperature
+let celsius=25;
+// Convert Celsius to Fahrenheit using the formula: F = (C * 9/5) + 32
+let fahrenheit=(celsius*9/5)+32
+
+
+// Print the result
+console.log(`${celsius}°C is equal to ${fahrenheit}°F`);
