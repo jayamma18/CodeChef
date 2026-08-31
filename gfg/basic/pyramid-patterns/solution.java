4c0aadc0809b38f204dd73c829a8e3b37e6947cd
jayamma18 <sakajayamma7@gmail.com>
2026-08-31 15:27:43 +0530

    ✅ Pyramid Pattern | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-31

---
+++ gfg/basic/pyramid-patterns/solution.java
@@ -0,0 +1,15 @@
+class GFG {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
