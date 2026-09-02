44dea1849d8e412b5372e050c7d2c15e56485d6e
jayamma18 <sakajayamma7@gmail.com>
2026-09-02 11:55:43 +0530

    ✅ VBHXB18 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/VBHXB18/solution.cpp
@@ -0,0 +1,25 @@
+  
+      const pourResult=await pourCoffee();
+      
+
+      console.log(brewResult);
+      const brewResult=await brewCoffee();
+      // complete the code here:
+  
+      console.log(grindResult);
+    try {
+      const grindResult = await grindCoffeeBeans();
+  async function placeOrder() {
+  
+  }
+    });
+      }, 500); // Simulate pouring for 0.5 seconds
+        resolve("Coffee poured!");
+      setTimeout(() => {
+    return new Promise(resolve => {
+  function pourCoffee() {
+  
+  }
+    });
+      }, 2000); // Simulate brewing for 2 seconds
+      console.log(pourResult);
