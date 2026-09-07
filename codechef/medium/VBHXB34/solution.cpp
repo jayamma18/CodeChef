9f7264f981a893ce47ea29fbbd7f9fb1b3800584
jayamma18 <sakajayamma7@gmail.com>
2026-09-07 15:09:33 +0530

    ✅ VBHXB34 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-07

---
+++ codechef/medium/VBHXB34/solution.cpp
@@ -0,0 +1,30 @@
+function runImmediate() {
+  setImmediate(() => {
+    console.log("setImmediate executed");
+  });
+}
+
+function runTimeout() {
+  setTimeout(() => {
+    console.log("setTimeout executed");
+  }, 1000);
+}
+
+function runLimitedInterval() {
+  let count = 0;
+  const intervalId = setInterval(() => {
+    count++;
+    console.log(`setInterval run ${count}`);
+    if (count === 2) {
+      clearInterval(intervalId);
+    }
+  }, 500);
+}
+
+module.exports = {
+  runImmediate,
+  runTimeout,
+  runLimitedInterval,
+};
+
+if (require.main === module) {
