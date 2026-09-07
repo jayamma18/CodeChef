16882f3588f40d1b2ef4af910c2bd4481954d243
jayamma18 <sakajayamma7@gmail.com>
2026-09-07 15:56:41 +0530

    ✅ VBHXB37 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-07

---
+++ codechef/medium/VBHXB37/solution.cpp
@@ -0,0 +1,21 @@
+function runOrderExample() {
+  console.log("Start");
+
+  process.nextTick(() => {
+    console.log("Next Tick");
+  });
+
+  Promise.resolve().then(() => {
+    console.log("Promise");
+  });
+
+  setImmediate(() => {
+    console.log("Immediate");
+  });
+
+  setTimeout(() => {
+    console.log("Timeout");
+  }, 0);
+
+  console.log("End");
+}
