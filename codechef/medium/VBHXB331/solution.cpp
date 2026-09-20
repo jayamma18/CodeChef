de5e48df97ce4f58ebd06f84899676cacff76710
jayamma18 <sakajayamma7@gmail.com>
2026-09-20 12:40:03 +0530

    ✅ VBHXB331 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-19

---
+++ codechef/medium/VBHXB331/solution.cpp
@@ -0,0 +1,13 @@
+// 1. Import the express library
+import express from 'express';
+
+// 2. Initialize the Express application
+const app = express();
+
+// 3. Define the PORT number
+// Remember, for consistency, we're using PORT 8080 for the backend.
+const PORT = 8080;
+
+// 4. Create a basic route for the root URL ('/')
+// When someone visits your backend's base URL, it should send a simple message.
+ app.get('/', (req, res) => {
