694d26c62cf919c9b05eac6fb090b9045534fea7
jayamma18 <sakajayamma7@gmail.com>
2026-09-21 16:04:32 +0530

    ✅ OJJAR47 | Medium | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/OJJAR47/solution.js
+++ codechef/medium/OJJAR47/solution.js
@@ -1,6 +1,9 @@
-const cartPrices = [29.99, 9.99, 4.99, 14.99, 25.00];
+const words = ["apple", "banana", "cherry", "watermelon", "kiwi"];
 
 // complete the code 
-// Using reduce to calculate the total price
-const totalPrice =cartPrices.reduce((accumulator,price)=>accumulator+price,0);
-console.log(totalPrice);
+
+
+const f=words.reduce((longest,current)=>{
+    return current.length>longest.length?current:longest;
+});
+console.log(f);
