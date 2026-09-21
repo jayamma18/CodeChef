16d8eee9be62116153409493e4642e27b885ad91
jayamma18 <sakajayamma7@gmail.com>
2026-09-21 16:01:46 +0530

    ✅ VBHXB334 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/medium/VBHXB334/solution.cpp
@@ -0,0 +1,4 @@
+MONGO_URI=mongodb+srv://sakajayamma2007_db_user:Jaya@cluster0.pjfd3i8.mongodb.net/?appName=cluster0 # <-- rpaste-your-mongo-uri-hereeplace with your actual URI
+PORT=8080
+JWT_SECRET=your_super_secret_jwt_key # <--- Make sure this is a strong, random string!
+
