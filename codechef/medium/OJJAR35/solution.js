df499f0aefc1184cfaa9c6cc8dab931eacbe1279
jayamma18 <sakajayamma7@gmail.com>
2026-09-21 15:09:13 +0530

    ✅ OJJAR35 | Medium | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/OJJAR35/solution.js
+++ codechef/medium/OJJAR35/solution.js
@@ -1,6 +1,6 @@
-const numbers = [1, 2, 3, 4, 5];
+const words = ['hello', 'world', 'javascript'];
 
 // complete the code 
-const squaredNumbers = numbers.map(num=>num*num); 
+const uppercaseWords=words.map(word=>word.toUpperCase()) ;
 
-console.log(squaredNumbers); 
+console.log(uppercaseWords); 
