8e8033804a1af5990a33521ae66dc2fda202b71c
jayamma18 <sakajayamma7@gmail.com>
2026-09-21 14:00:16 +0530

    ✅ VBHXB337 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/medium/VBHXB337/solution.cpp
@@ -0,0 +1,11 @@
+
+const MyBlogs = () => {
+    return (
+        <div className="container">
+            <h1>My Blogs</h1>
+            <p>This page will show only the blogs you have created.</p>
+        </div>
+    );
+};
+
+export default MyBlogs;
