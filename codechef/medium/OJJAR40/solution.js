862dcfc8d2a3bb29cc8b8907069fb9e4979a4740
jayamma18 <sakajayamma7@gmail.com>
2026-09-21 15:27:06 +0530

    ✅ OJJAR40 | Medium | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/medium/OJJAR40/solution.js
@@ -0,0 +1,14 @@
+let originalNumbers = [1, 5, 10];
+
+// Create an empty array to store the results
+
+// Iterate over the original numbers
+let doubledNumbers=[];
+// Calculate the doubled value
+
+// Add the doubled value to our new array
+originalNumbers.forEach(num=> {
+    doubledNumbers.push(num*2);
+});
+
+console.log("Doubled:",doubledNumbers );
