89bde6d9dabf38d93a0c28ad329f078f098d3bdf
jayamma18 <sakajayamma7@gmail.com>
2026-09-23 16:16:03 +0530

    ✅ OJJAR136 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/OJJAR136/solution.cpp
@@ -0,0 +1,25 @@
+import { NavLink, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+
+function App() {
+  return (
+    <>
+      <header>
+        <nav className="nav">
+          <NavLink to="/" end>Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </nav>
+      </header>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </>
+  )
+}
+
+export default App
