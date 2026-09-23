50b2d204fa886af77e9e1a1cba4b5df7bdcf55de
jayamma18 <sakajayamma7@gmail.com>
2026-09-23 12:34:43 +0530

    ✅ OJJAR56 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/OJJAR56/solution.cpp
@@ -0,0 +1,19 @@
+import React from 'react';
+export default function App() {
+return (
+<div
+style={{
+width: '200px',
+height: '100px',
+backgroundColor: 'lightblue',
+color: 'white',
+display: 'flex',
+justifyContent: 'center',
+alignItems: 'center',
+fontSize: '18px'
+}}
+>
+Styled Box
+</div>
+);
+}
